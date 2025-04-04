210b8fb9a823a6e2bc23aa27b34263be2a32859a
chanchahal007 <149024754+chanchahal007@users.noreply.github.com>
2025-04-04 12:16:02 -0700

    Update notes.py

--- backend/app/notes.py
+++ backend/app/notes.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from .utils import verify_jwt_token
 
 router = APIRouter()
 
-@router.get("/")
+@router.get("/", dependencies=[Depends(verify_jwt_token)])
 def list_notes():
-    return {"notes": []}
+    return {"notes": ["Sample note 1", "Sample note 2"]}
