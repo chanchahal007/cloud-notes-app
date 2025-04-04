2aa73dcfb20c370cb6c8a91becc137d250e664ea
chanchahal007 <149024754+chanchahal007@users.noreply.github.com>
2025-04-04 12:17:34 -0700

    Update __main__.py

--- backend/app/__main__.py
+++ backend/app/__main__.py
@@ -4,10 +4,10 @@ from .notes import router as notes_router
 
 app = FastAPI()
 
-# Routes
-app.include_router(auth_router, prefix="/auth")
-app.include_router(notes_router, prefix="/notes")
-
 @app.get("/")
 def root():
     return {"message": "Cloud Notes API is running"}
+
+app.include_router(auth_router, prefix="/auth")
+app.include_router(notes_router, prefix="/notes")
+
